87dfda2f8dfe7401efb71b32b80b53a0fb55b4ff
izsal <izsalavandi@gmail.com>
2020-10-18 23:03:15 +0700

    first commit

--- service-worker.js
+++ service-worker.js
@@ -1,80 +1,58 @@
-const CACHE_NAME = "footbal-data-PWA";
-var urlsToCache = [
-  "/",
-  "/nav.html", "/index.html",
-  "/team.html", "/manifest.json",
-  "/bola.png", "/images/premier.png",
-  "/images/izsal_qurlinas_afandi.jpg",
-  "/images/laliga.png", "/pages/home.html",
-  "/pages/premier.html", "/pages/laliga.html",
-  "/pages/favorite.html", "/css/materialize.min.css",
-  "/js/materialize.min.js", "/js/nav.js",
-  "/js/football_api.js", "/js/idb.js",
-  "/js/main.js", "/js/database.js"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+ 
+if (workbox)
+  console.log(`Workbox berhasil dimuat`);
+else
+  console.log(`Workbox gagal dimuat`);
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+  workbox.precaching.precacheAndRoute([
+    { url: '/index.html', revision: '1' },
+    { url: '/team.html', revision: '1' },
+    { url: '/manifest.json', revision: '1' },
+    { url: '/bola.png', revision: '1' },
+    { url: '/css/materialize.min.css', revision: '1' },
+]);
 
-self.addEventListener("fetch", function(event) {
-  var base_url = "https://api.football-data.org/v2/";
-
-  if (event.request.url.indexOf(base_url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(response) {
-          cache.put(event.request.url, response.clone());
-          return response;
-        })
+workbox.routing.registerRoute(
+    new RegExp('/pages/'),
+      workbox.strategies.staleWhileRevalidate({
+          cacheName: 'pages'
       })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-        return response || fetch (event.request);
-      })
-    )
-  }
-});
-
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
-});
+
+workbox.routing.registerRoute(
+  new RegExp('/images/'),
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'images'
+    })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('/js/'),
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'javascript'
+    })
+);
 
 
-self.addEventListener('push', function(event) {
-  var body;
-  if (event.data) {
-    body = event.data.text();
-  } else {
-    body = 'Push message no payload';
-  }
-  var options = {
-    body: body,
-    icon: 'img/notification.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: 1
+
+  self.addEventListener('push', function (event) {
+    var body;
+    if (event.data) {
+      body = event.data.text();
+    } else {
+      body = 'Push message no payload';
     }
-  };
-  event.waitUntil(
-    self.registration.showNotification('Push Notification', options)
-  );
-});
+    var options = {
+      body: body,
+      icon: 'bola.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      }
+    };
+    event.waitUntil(
+      self.registration.showNotification('Push Notification', options)
+    );
+  });
